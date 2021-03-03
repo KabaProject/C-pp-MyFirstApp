da7df4b0563a24fbb4ecf6101e8a06dd5cd1ec4c
AlbertHdez <mariohdez_proyectos@outlook.com>
2021-03-02 21:45:10 -0600

    Add project files.

---
+++ MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // DataTypes
+
+    // int yearOfBirth = 1995;
+    // char gender = 'M';
+    // bool isOlderThan18 = true;
+    // float averageGrade = float(2000 - yearOfBirth - 0.5);
+    // double balance = 4629384134;
+       
+    // cout << "Size of int is " << sizeof(int) << " bytes\n";
+    // cout << "Int min value is " << INT_MIN << " and max is " << INT_MAX << endl;
+    // cout << "Unsigned Int max value is " << UINT_MAX << endl;
+
+
+    system("pause>0");
+}
